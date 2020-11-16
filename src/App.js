36b7864fb261f5b900ef29d6722102767880e48a
vproskurin <vladimir.proskurin@gmail.com>
2020-11-15 16:30:24 -0800

    git init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+const Pet = (props) => {
+  return React.createElement(
+    "div", {}, [
+      React.createElement("h1", {}, props.name),
+      React.createElement("h2", {}, props.animal),
+      React.createElement("h2", {}, props.breed),
+    ]
+  )
+}
+
+const App = () => {
+    return React.createElement(
+      "div",
+      {},
+      [
+        React.createElement("h1", {}, "Adopt me"),
+        React.createElement(Pet, {name: "Henry", animal: "Dog", breed: "Frenchie"}),
+        React.createElement(Pet, {name: "Boby", animal: "Cat", breed: "Exotic"}),
+        React.createElement(Pet, {name: "Buba", animal: "Bird", breed: "Parakeet"}),
+    ]
+    );
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
