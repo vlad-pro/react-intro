4383cbfaf66a2c7cc66ef054d4dc6e3492b17efb
vproskurin <vladimir.proskurin@gmail.com>
2020-11-20 23:57:42 -0800

    useEffect added

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useEffect, useState } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seatle, WA");
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  // const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
-  // const [breeds, setBreeds] = useState([]);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+  const [breeds, setBreeds] = useState([]);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({breeds: apiBreeds}) => {
+      const breedStrings = apiBreeds.map(({name}) => name)
+      setBreeds(breedStrings)
+    }, error => console.error(error))
+  }, [setBreed, animal, setBreeds]) // if you want to run effect once, then DependencyList should be [] empty. 
 
   return (
     <div className="search-params">
@@ -21,7 +31,7 @@ const SearchParams = () => {
           />
         </label>
         <AnimalDropdown />
-        {/* <BreedDropdown /> */}
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
