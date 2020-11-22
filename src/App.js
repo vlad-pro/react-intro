7e50f493801555b93f951be72ee62690a88853f7
vproskurin <vladimir.proskurin@gmail.com>
2020-11-22 01:59:07 -0800

    reach router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
+import { Link, Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./details";
 
 const App = () => {
   return (
     // Strict mode is just for future proof the app - give warnings when some depricated fucntion is used
     <React.StrictMode>
       <div>
-        <h1>Adopt me</h1>
-        <SearchParams />
+        <header>
+          <Link to="/">
+            <h1>Adopt me</h1>
+          </Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
