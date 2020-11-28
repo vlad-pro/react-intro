ce2dd635169ac46dad34835a1e3d15a976147527
vproskurin <vladimir.proskurin@gmail.com>
2020-11-27 17:11:19 -0800

    Portals and Refs

--- src/details.js
+++ src/details.js
@@ -1,5 +1,7 @@
 import React from "react";
 import pet from "@frontendmasters/pet";
+import { navigate } from "@reach/router";
+import  Modal from "./Modal";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
@@ -10,12 +12,16 @@ class Details extends React.Component {
 
     this.state = {
       loading: true,
+      showModal: false,
     };
+    this.toggleModal = this.toggleModal.bind(this)
+    this.adopt = this.adopt.bind(this)
   }
   componentDidMount() {
     // throw new Error("error to chek the handling") // can be used to check error boundry handling in components
     pet.animal(this.props.id).then(({ animal }) => {
       this.setState({
+        url: animal.url,
         name: animal.name,
         animal: animal.type,
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
@@ -27,12 +33,23 @@ class Details extends React.Component {
     }, console.error);
   }
 
+  toggleModal() {this.setState({ showModal: !this.state.showModal })};
+  adopt() {navigate(this.state.url)};
+
   render() {
     if (this.state.loading) {
       return <h1>Loading...</h1>;
     }
 
-    const { animal, breed, location, description, name, media } = this.state;
+    const {
+      animal,
+      breed,
+      location,
+      description,
+      name,
+      media,
+      showModal,
+    } = this.state;
 
     return (
       <div className="details">
@@ -45,12 +62,26 @@ class Details extends React.Component {
               // themeHook[0] because we are provided the theme and the setTheme function.
               // These we are getitng from the context that we set in the .Provided in the App.js
               // This is the example where hooks are a great combo with Contex, even though I do not qite get it.
-              <button style={{ backgroundColor: themeHook[0] }}>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: themeHook[0] }}
+              >
                 Adopt {name}
               </button>
             )}
           </ThemeContext.Consumer>
           <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <button onClick={this.adopt}>Yes</button>
+                  <button onClick={this.toggleModal}>No, not ready yet</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
